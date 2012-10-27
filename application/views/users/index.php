476ae5c7c964084beb05fe3af9f288291fd8622b
Pook <veeraya.pupat@gmail.com>
2012-10-28 04:41:41 +0800

    'edit user' instead of 'view user' on user/index

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,9 +1,12 @@
 <?php foreach ($users as $user): ?>
 
-    <h2>Email: <?php echo $user['email'] ?></h2>
-    <div id="main">
-        Username: <?php echo $user['username'] ?>
-    </div>
-    <p><a href="users/view/<?php echo $user['username'] ?>">View user</a></p>
+    <b>Email:</b> <?php echo $user['email'] ?><br />
+    <b>Username:</b> <?php echo $user['username'] ?><br />
+    <?php if ($user['username'] == $this->session->userdata('username')){
+    	$editUrl = base_url()."index.php/users/edit/".$user['username'];
+    	echo "<a href=\"$editUrl\">Edit profile</a><br/><br/>";
+    	}
+    	else echo "<br />";
+    ?>
 
 <?php endforeach ?>
