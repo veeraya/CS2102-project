7f89e2507be5cf624ad4694480be1bc3d5a256aa
Pook <veeraya.pupat@gmail.com>
2012-10-28 05:20:00 +0800

    change base_url in config to ''

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/cs2102/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
