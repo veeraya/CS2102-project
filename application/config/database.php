0dcface3df5c5382e9a47121e58afe738db1d6ee
Pook <veeraya.pupat@gmail.com>
2012-10-28 05:24:30 +0800

    change config file for remote server

--- application/config/database.php
+++ application/config/database.php
@@ -45,24 +45,40 @@
 | the active record class
 */
 
-$active_group = 'default';
-$active_record = TRUE;
+$active_group = 'remote';
+$active_record = FALSE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'cs2102';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['local']['hostname'] = 'localhost';
+$db['local']['username'] = 'root';
+$db['local']['password'] = '';
+$db['local']['database'] = 'cs2102';
+$db['local']['dbdriver'] = 'mysql';
+$db['local']['dbprefix'] = '';
+$db['local']['pconnect'] = TRUE;
+$db['local']['db_debug'] = TRUE;
+$db['local']['cache_on'] = FALSE;
+$db['local']['cachedir'] = '';
+$db['local']['char_set'] = 'utf8';
+$db['local']['dbcollat'] = 'utf8_unicode_ci';
+$db['local']['swap_pre'] = '';
+$db['local']['autoinit'] = TRUE;
+$db['local']['stricton'] = FALSE;
+
+$db['remote']['hostname'] = 'pook.in';
+$db['remote']['username'] = 'root';
+$db['remote']['password'] = '';
+$db['remote']['database'] = 'pookin_cs2102';
+$db['remote']['dbdriver'] = 'mysql';
+$db['remote']['dbprefix'] = '';
+$db['remote']['pconnect'] = TRUE;
+$db['remote']['db_debug'] = TRUE;
+$db['remote']['cache_on'] = FALSE;
+$db['remote']['cachedir'] = '';
+$db['remote']['char_set'] = 'utf8';
+$db['remote']['dbcollat'] = 'utf8_unicode_ci';
+$db['remote']['swap_pre'] = '';
+$db['remote']['autoinit'] = TRUE;
+$db['remote']['stricton'] = FALSE;
 
 
 /* End of file database.php */
