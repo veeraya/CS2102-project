aa1f156bfaf92981b261855e895386cc57171372
Shubhendra Agrawal <a0065494@nus.edu.sg>
2012-10-31 17:47:52 +0800

    integrating the new theme with the codeignitor framework

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,9 +19,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('templates/header');
+		//$this->load->view('templates/header');
 		$this->load->view('welcome_message');
-		$this->load->view('templates/footer');
+		//$this->load->view('templates/footer');
 	}
 }
 
