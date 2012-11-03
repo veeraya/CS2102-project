c40e55bed7a6330766f8e10d4ba75d67b9c17fc5
Shubhendra Agrawal <a0065494@nus.edu.sg>
2012-11-03 17:11:04 +0800

    integrated the login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -12,39 +12,43 @@
 </form>
   No account? <a href="<?php echo base_url(); ?>index.php/users/create">Sign up here.</a>
 </div> -->
-
-<div id= "homepage" class= "clear">
-	<div class = "f1_left">
+<div class="wrapper">
+  <div id="footer" class="clear" background = >
+    <div class="fl_left">
+      <div class="about_us border">
 		<h2>Sign In</h2>
 		<form action="<?php echo base_url(); ?>index.php/auth/processLogin" method="post" name="processLogin">
 	
 			<?php if (!is_null($errorMsg)) echo $errorMsg; ?>
 			<label for="email">Email</label>
-			<input type="text" name="email" id="email"/><br />
+			<input type="text" name="email" value="Email Address"/><br />
 
 			<label for="password">Password</label>
-			<input type="password" name="password" id="password" /><br />
+			<input type="password" name="password" value="password" /><br />
 
-			<input type="submit" value="Login" />
+			<button type="submit" value="submit"><span>LOGIN</span></button>
 		</form>
 	</div>
-	<div class = "f1_right">
+</div>
+	<div class="f1_right">
+      <div id="contact" class="clear">
 		<h2>Sign Up</h2>
 			<?php echo validation_errors(); ?>
 
 			<?php echo form_open('users/create') ?>
 
 			<label for="title">Email</label> 
-			<input type="text" name="email" /><br />
-
+			<input type="text" name="email" value="Email Address" />
 			<label for="username">Username</label> 
-			<input type="text" name="username" /><br />
-
+			<input type="text" name="username" value="Username" />
 			<label for="password">Password</label> 
-			<input type="password" name="password" /><br />
+			<input type="password" name="password" value="Password" />
 
-			<input type="submit" name="submit" class="btn" value="Sign up" /> 
+			<button type="submit" value="submit" float = "right"><span>SIGN UP</span></button> 
 
 		</form>
 	</div>
 </div>
+</div>
+</div>
+<div><hr></div>
