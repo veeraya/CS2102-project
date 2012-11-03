a033e61d819ad1c8a5552a0be290a51ae4786ff1
Pook <veeraya.pupat@gmail.com>
2012-11-04 05:04:34 +0800

    fixed layout of create/edit review form

--- application/views/reviews/create.php
+++ application/views/reviews/create.php
@@ -1,31 +1,30 @@
 <div class="container_12">
 <form action="<?php echo base_url(); ?>index.php/restaurants/<? echo $restaurantUrl ?>/processCreateReview" method="post" name="processCreateReview">
 	<h2>Submit a review</h2>
-	<label for="title">Title</label>
-	<input type="text" name="title" id="title" class="input-xxlarge"/><br />
-	<label for="content">Content</label>
-	<textarea name="content" id="content" rows="8" class="input-xxlarge"></textarea><br />
-	<label for="foodRating">Food Rating</label>
-	<select name="foodRating"> 
-	  <option value="1" >1</option>
-	  <option value="2" >2</option>
-	  <option value="3" >3</option>
-	  <option value="4" >4</option>
-	  <option value="5" >5</option>
-	</select><br />
-	<label for="serviceRating">Service Rating</label>
-	<select name="serviceRating"> 
-	  <option value="1" >1</option>
-	  <option value="2" >2</option>
-	  <option value="3" >3</option>
-	  <option value="4" >4</option>
-	  <option value="5" >5</option>
-	</select><br />
+	<label for="title">Title</label><br />
+		<input type="text" name="title" id="title" class="input-xxlarge"/><br /><br />
+		<label for="content">Content</label><br />
+		<textarea name="content" cols="130" rows="15"></textarea><br /><br />
+		<label for="foodRating">Food Rating</label><br />
+		<select name="foodRating" id="foodRating"> 
+		  <option value="1" >1</option>
+		  <option value="2" >2</option>
+		  <option value="3" >3</option>
+		  <option value="4" >4</option>
+		  <option value="5" >5</option>
+		</select><br /><br />
+		<label for="serviceRating">Service Rating</label><br />
+		<select name="serviceRating" id="serviceRating"> 
+		  <option value="1" >1</option>
+		  <option value="2" >2</option>
+		  <option value="3" >3</option>
+		  <option value="4" >4</option>
+		  <option value="5" >5</option>
+		</select><br /><br />
 
-	<label for="recommend">Would you recommend this restaurant to your friends?</label>
-	<input type="radio" name="recommend" value="1">Yes<br />
-	<input type="radio" name="recommend" value="0">No<br /><br />
-
+		<label for="recommend">Would you recommend this restaurant to your friends?</label><br />
+		<input type="radio" name="recommend" id="doRecommend" value="1">Yes<br />
+		<input type="radio" name="recommend" id="dontRecommend" value="0">No<br /><br />
 
 	<input type="submit" class="btn" value="Submit" />
 </form>
--- application/views/reviews/edit.php
+++ application/views/reviews/edit.php
@@ -2,28 +2,28 @@
 <div class="container_12">
 <form action="<?php echo base_url(); ?>index.php/reviews/processEdit" method="post" name="processEdit">
 	<h2>Edit your review</h2>
-	<label for="title">Title</label>
-	<input type="text" name="title" id="title" class="input-xxlarge" value="<?php echo $review['title']; ?>"/><br />
-	<label for="content">Content</label>
-	<textarea name="content" id="content" rows="8" class="input-xxlarge" ><?php echo $review['content']; ?></textarea><br />
-	<label for="foodRating">Food Rating</label>
+	<label for="title">Title</label><br />
+	<input type="text" name="title" id="title" class="input-xxlarge" value="<?php echo $review['title']; ?>"/><br /><br />
+	<label for="content">Content</label><br />
+	<textarea name="content" cols="130" rows="15"><?php echo $review['content']; ?></textarea><br /><br />
+	<label for="foodRating">Food Rating</label><br />
 	<select name="foodRating" id="foodRating"> 
 	  <option value="1" >1</option>
 	  <option value="2" >2</option>
 	  <option value="3" >3</option>
 	  <option value="4" >4</option>
 	  <option value="5" >5</option>
-	</select><br />
-	<label for="serviceRating">Service Rating</label>
+	</select><br /><br />
+	<label for="serviceRating">Service Rating</label><br />
 	<select name="serviceRating" id="serviceRating"> 
 	  <option value="1" >1</option>
 	  <option value="2" >2</option>
 	  <option value="3" >3</option>
 	  <option value="4" >4</option>
 	  <option value="5" >5</option>
-	</select><br />
+	</select><br /><br />
 
-	<label for="recommend">Would you recommend this restaurant to your friends?</label>
+	<label for="recommend">Would you recommend this restaurant to your friends?</label><br />
 	<input type="radio" name="recommend" id="doRecommend" value="1">Yes<br />
 	<input type="radio" name="recommend" id="dontRecommend" value="0">No<br /><br />
 
